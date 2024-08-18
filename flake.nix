715f409805e82b752c529a5353dd6295012c9466
Igor Sidorov <igor.sidorov@binarycode.ru>
2024-08-18 11:31:16 +0300

    use preset

--- flake.nix
+++ flake.nix
@@ -2,13 +2,7 @@
   outputs = _: let
     module = import ./module.nix;
   in {
-    nixosModules = {
-      default  = module;
-      starship = module;
-    };
-    darwinModules = {
-      default  = module;
-      starship = module;
-    };
+    nixosModules.default = module;
+    darwinModules.default = module;
   };
 }
--- module.nix
+++ module.nix
@@ -1,7 +1,40 @@
-{ pkgs, ... }: {
-  environment = {
-    etc."starship.toml".source = ./starship.toml;
+{
+  programs.starship = {
+    enable = true;
 
-    systemPackages = [ pkgs.starship ];
+    presets = [ "plain-text-symbols" ];
+
+    settings = {
+      battery.disable = true;
+
+      directory = {
+        truncation_length = 8;
+        truncation_symbol = ".../";
+        read_only = " RO";
+      };
+
+      # git_status is painfully slow in large repos
+      git_status.disabled = true;
+
+      hostname = {
+        format = "@[$hostname]($style):";
+        ssh_only = false;
+      };
+
+      status = {
+        disabled = false;
+        symbol = "E";
+      };
+
+      time = {
+        disabled = false;
+        format = "[$time]($style) ";
+      };
+
+      username = {
+        format = "[$user]($style)";
+        show_always = true;
+      };
+    };
   };
 }
